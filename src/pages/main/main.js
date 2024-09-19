154ba4c41ea1c6304f10a919bea643460535a1d5
Graf <graf-n7@gmail.com>
2024-09-19 23:01:35 +0400

    Реализовать поиск на главной

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,40 +1,59 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { useServerRequest } from '../../hooks';
-import { Pagination, PostCard } from './components';
+import { Pagination, PostCard, Search } from './components';
 import { PAGINATION_LIMIT } from '../../constants';
 import { getLastPageFromLinks } from './utils/get-last-page-from-links';
+import { debounce } from './utils/debounce';
 
 const MainContainer = ({ className }) => {
 	const requestServer = useServerRequest();
 	const [page, setPage] = useState(1);
 	const [posts, setPosts] = useState([]);
 	const [lastPage, setLastPage] = useState([]);
+	const [shouldSearch, setShouldSearch] = useState(false);
+	const [searchPhrase, setSearchPhrase] = useState('');
 
 	useEffect(() => {
-		requestServer('fetchPosts', page, PAGINATION_LIMIT).then(
+		requestServer('fetchPosts', searchPhrase, page, PAGINATION_LIMIT).then(
 			({ resp: { posts, links } }) => {
 				setPosts(posts);
 				setLastPage(getLastPageFromLinks(links));
 			},
 		);
-	}, [requestServer, page]);
+	}, [requestServer, page, shouldSearch]);
+
+	const startDelayedSearch = useMemo(() => debounce(setShouldSearch, 2000), []);
+
+	const onSearch = ({ target }) => {
+		setSearchPhrase(target.value);
+		startDelayedSearch(!shouldSearch);
+	};
 
 	return (
 		<div className={className}>
-			<div className="post-list">
-				{posts.map(({ id, title, publishedAt, commentsCount, imageUrl }) => (
-					<PostCard
-						key={id}
-						id={id}
-						title={title}
-						imageUrl={imageUrl}
-						publishedAt={publishedAt}
-						commentsCount={commentsCount}
-					/>
-				))}
+			<div className="posts-and-search">
+				<Search searchPhrase={searchPhrase} onChange={onSearch} />
+				{posts.length > 0 ? (
+					<div className="post-list">
+						{posts.map(
+							({ id, title, publishedAt, commentsCount, imageUrl }) => (
+								<PostCard
+									key={id}
+									id={id}
+									title={title}
+									imageUrl={imageUrl}
+									publishedAt={publishedAt}
+									commentsCount={commentsCount}
+								/>
+							),
+						)}
+					</div>
+				) : (
+					<div className="no-posts-found">Статьи не найдены</div>
+				)}
 			</div>
-			{lastPage > 1 && (
+			{lastPage > 1 && posts.length > 0 && (
 				<Pagination page={page} lastPage={lastPage} setPage={setPage} />
 			)}
 		</div>
@@ -42,9 +61,20 @@ const MainContainer = ({ className }) => {
 };
 
 export const Main = styled(MainContainer)`
+	& .posts-and-search {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
 	& .post-list {
 		display: flex;
 		flex-wrap: wrap;
-		padding: 20px;
+		padding: 20px 20px 80px;
+	}
+
+	& .no-posts-found {
+		font-size: 18px;
+		margin-top: 40px;
+		text-align: center;
 	}
 `;
